1c22c9b47193569f96d8d60e2e9093a9fe7c440f
xjones <jonescyna@gmail.com>
2022-05-17 17:35:01 +0800

    update dev toolchain

--- deploy/01_Greeter.ts
+++ deploy/01_Greeter.ts
@@ -1,6 +1,5 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 import {DeployFunction} from 'hardhat-deploy/types';
-import {Greeter} from "../typechain";
 
 // import {parseEther} from 'ethers/lib/utils';
 
